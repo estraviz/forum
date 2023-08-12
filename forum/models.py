2e2108c8aaf745415a352006a961f47299e6e217
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-12 10:05:52 +0200

    Created project, forum app, and Post and Comment models

---
+++ forum/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name="posts")
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, null=True, blank=True, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField(null=True, blank=True)
